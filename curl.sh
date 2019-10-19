ad02d78598a1da448136bcc1f7793d77d1645b67
jmshanahan <comeraghsolutions@gail.com>
2019-10-19 12:20:52 +0100

    Added remote git library

--- curl.sh
+++ curl.sh
@@ -1,2 +1,5 @@
 
 curl -X POST -H "Content-Type:application/json" http://localhost:3000/messages -d '{"text":"Hi again, World"}'
+
+
+curl -X DELETE http://localhost:3000/messages/1
--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import "dotenv/config";
 import cors from "cors";
+import models from "./models";
 import uuidv4 from "uuid/v4";
 import express from "express";
-
-let users = {
-  1: {
-    id: "1",
-    username: "Robin Wieruch"
-  },
-  2: {
-    id: "2",
-    username: "Dave Davids"
-  }
-};
-let messages = {
-  1: {
-    id: "1",
-    text: "Hello World",
-    userId: "1"
-  },
-  2: {
-    id: "2",
-    text: "By World",
-    userId: "2"
-  }
-};
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -33,29 +11,49 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1]
+  };
+  next();
+});
+
+app.get("/session", (req, res) => {
+  return res.send(req.context.models.users[req.context.me.id]);
+});
 app.get("/", (req, res) => {
   res.send("Received a POST HTTP mehtod");
 });
 app.get("/users", (req, res) => {
-  res.send(Object.values(users));
+  res.send(Object.values(req.context.models.users));
 });
 app.get("/users/:userId", (req, res) => {
-  res.send(Object.values(users[req.params.userId]));
+  res.send(Object.values(req.context.models.users[req.params.userId]));
 });
 app.get("/messages", (req, res) => {
-  return res.send(Object.values(messages));
+  return res.send(Object.values(req.context.models.messages));
 });
 app.post("/messages", (req, res) => {
   const id = uuidv4();
   const message = {
     id,
-    text: req.body.text
+    text: req.body.text,
+    userId: req.context.me.id
   };
-  messages[id] = message;
-  return res.send(Object.values(messages));
+  req.context.models.messages[id] = message;
+  return res.send(message);
 });
 app.get("/messages/:messageId", (req, res) => {
-  return req.send(messages[req.params.messageId]);
+  return res.send(req.context.models.messages[req.params.messageId]);
+});
+app.delete("/messages/:messageId", (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = req.context.models.messages;
+  req.context.models.messages = otherMessages;
+  return res.send(message);
 });
 
 app.post("/", (req, res) => {
@@ -68,5 +66,4 @@ app.put("/", (req, res) => {
 app.delete("/", (req, res) => {
   res.send("Received a delete HTTP mehtod");
 });
-
 app.listen(port, () => console.log("Example app listening on port 3000"));
